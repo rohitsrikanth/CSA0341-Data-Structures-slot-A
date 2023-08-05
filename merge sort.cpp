2647e056747be16b0a575e936ca7fbd4f03616b4
rohitsrikanth <120450374+rohitsrikanth@users.noreply.github.com>
2023-08-05 13:20:08 +0530

    Add files via upload

---
+++ merge sort.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main(){
+int merge(int a[], int beg, int mid, int end){    
+    int i, j, k;  
+    int n1 = mid - beg + 1;    
+    int n2 = end - mid;         
+    int LeftArray[n1], RightArray[n2];
+    for (int i = 0; i < n1; i++)    
+    LeftArray[i] = a[beg + i];    
+    for (int j = 0; j < n2; j++)    
+    RightArray[j] = a[mid + 1 + j];    
+    i = 0;
+    j = 0;  
+    k = beg;  
+    while (i < n1 && j < n2){    
+        if(LeftArray[i] <= RightArray[j])    {    
+            a[k] = LeftArray[i];    
+            i++;    
+        }else{    
+            a[k] = RightArray[j];    
+            j++;    
+        }    
+        k++;    
+    }    
+    while (i<n1){    
+        a[k] = LeftArray[i];    
+        i++;    
+        k++;    
+    }   
+    while (j<n2){    
+        a[k] = RightArray[j];    
+        j++;    
+        k++;    
+    }    
+}
+}
